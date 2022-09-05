e7e8c245db1b8940425f86030437a5661c44e272
Daniel Ditlow <danielditlow@gmail.com>
2022-09-04 22:06:42 -0600

    UPdate

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import page
 # Test #1
 
 
@@ -19,7 +19,7 @@ class TCGPlayercomSearch(unittest.TestCase):
 
     def test_search_in_tcgplayer_com(self):
         # Load mainpage
-        main_page = page.MainPage(self.driver)
+        main_page = MainPage(self.driver)
         # Checks if the word "Python" is in title
         self.assertTrue(main_page.is_title_matches(
         ), "TCGplayer: Online Store for Collectible Trading Card Games")
