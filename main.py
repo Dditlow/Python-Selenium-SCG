c33873184143b9471ec4a5fe1f0f95687878faca
Daniel Ditlow <danielditlow@gmail.com>
2022-09-05 08:04:15 -0600

    updated syntax

--- main.py
+++ main.py
@@ -2,28 +2,27 @@
 from importlib.resources import path
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import page
 # Test #1
 
 
 class TCGPlayercomSearch(unittest.TestCase):
-    """A sample test class to show how page object works"""
 
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.get("http://www.tcgplayer.com")
         self.driver.implicitly_wait(10)
 
-    def test_search_in_tcgplayer_com(self):
+    def test_print_in_tcgplayer_com(self):
         # Load mainpage
-        main_page = MainPage(self.driver)
+        main_page = page.MainPage(self.driver)
+        print(test)
+        
+    def test_title_search_in_tscgplayer_com():    
         # Checks if the word "Python" is in title
         self.assertTrue(main_page.is_title_matches(
         ), "TCGplayer: Online Store for Collectible Trading Card Games")
-        print("Testing")
+        print(driver.main_page.title)
 
     def tearDown(self):
         self.driver.close()
--- page.py
+++ page.py
@@ -1,2 +1,25 @@
 from element import BasePageElement
 from locators import MainPageLocators
+
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+class MainPage(BasePage):
+    search_text_element = SearchTextElement()
+
+    def is_title_matches(self):
+        return "TCGplayer: Online Store for Collectible Trading Card Games" in self.driver.title
+
+    def click_go_button(self):
+        """Triggers the search"""
+
+        element = self.driver.find_element(*MainPageLocators.GO_BUTTON)
+        element.click()
+
+class SearchResultsPage(BasePage):
+    """Search results page action methods come here"""
+
+    def is_results_found(self):
+
+        return "No results found." not in self.driver.page_source
